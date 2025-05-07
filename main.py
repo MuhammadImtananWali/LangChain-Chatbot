cdc5411b4d04594409af2043c47b3d670958505f
MuhammadImtananWali <contact.imtanan@gmail.com>
2025-05-07 16:02:21 +0500

    Integrated LangSmith tracking and some other refactoring.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import os
+import getpass
+
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
